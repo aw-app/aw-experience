40d49a21aa9212e0a10756a2eaf6da8789a0849d
John Doe <johndoe@example.com>
2019-01-20 18:13:02 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FadeInUp } from 'animate-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home } from './Home/Home';
+import { About } from './About/About';
+import { Contact } from './Contact/Contact';
+import { NavBar } from './NavBar/NavBar';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
+    );
   }
 }
 
